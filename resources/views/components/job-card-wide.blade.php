442d6dde5118c8654cf1e19edb0dce900f5bb759
hajricod <hajricod@gmail.com>
2024-09-05 19:10:33 +0400

    layout

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,13 @@
+<div class="p-4 bg-white/20 rounded-xl flex flex-col text-center">
+    <div class="self-start text-sm">Laracasts</div>
+    <div class="py-8 font-bold">
+        <h3>Video Producer</h3>
+        <p>Full time from $70,000</p>
+    </div>
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <x-tag>Tag</x-tag>
+        </div>
+        <img src="https://placehold.co/42x42" alt="" class="rounded-xl">
+    </div>
+</div>
