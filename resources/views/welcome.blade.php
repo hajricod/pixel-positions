9d73eea0723107b4c0c7c120debab68409365276
hajricod <hajricod@gmail.com>
2024-09-10 07:37:43 +0400

    style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,12 @@
 <x-layout>
     <div class="space-y-10">
-        <section>
+        <section class="text-center pt-6">
+            <h1 class="font-bold text-4xl">Let's Find You a Great Job</h1>
+            <form action="" class="mt-6">
+                <input type="text" name="search" id="search" placeholder="Web Developer..." class="rounded-xl bg-white/15 border border-white/10 px-5 py-4 w-full max-w-xl">
+            </form>
+        </section>
+        <section class="pt-10">
             <x-section-heading>Featured Jobs</x-section-heading>
             <div class="grid lg:grid-cols-3 gap-8 mt-6">
                 <x-job-card></x-job-card>
@@ -19,7 +25,7 @@
                 <x-tag>Tag</x-tag>
             </div>
         </section>
-        <section>
+        <section class="pb-6">
             <x-section-heading>Recent Jobs</x-section-heading>
             <div class="mt-6 space-y-2">
                 <x-job-card-wide />
